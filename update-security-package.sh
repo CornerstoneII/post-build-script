15c6336c1a9b16e24774ba5ccdb89c94b4fedda4
CornerstoneII <seunmyles@gmail.com>
2022-11-28 12:14:25 -0500

    Add update security patch

---
+++ update-security-package.sh
@@ -0,0 +1,11 @@
+# Run the following apt command
+sudo apt update
+
+# List available security patches or updates:
+sudo apt list --upgradable
+
+# Apply those security patches on a Debian/Ubuntu server
+sudo apt upgrade
+
+# Reboot the system if Linux kernel was updated or patched for security issues:
+sudo reboot
